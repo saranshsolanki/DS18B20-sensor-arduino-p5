6cf876525fb2eb5e66ff766f883b0b30bcf5f791
Saransh <saransh.solanki@gmail.com>
2019-03-16 21:45:24 -0700

    version 2

--- kidsteam/js/sketch.js
+++ kidsteam/js/sketch.js
@@ -31,19 +31,17 @@ function setup() {
 
   currentTemp = 655;
   // timeInc = windowWidth/(5*60*10);
-  timeInc = windowWidth/(2*60*10);
+  timeInc = windowWidth/(2*6*10);
   currentTime = 2*timeInc;
 
   var currentTempOld = 655;
 
   if(sessionStorage.getItem("steelTemperature") == 'true'){
-    console.log("steel");
-
+    
     while(currentTime < windowWidth){
 
      var newTempOld = sessionStorage.getItem("steel:"+currentTime);
-      console.log("here");
-      console.log(newTempOld);
+     
       stroke(0,204,255); //stroke color
       strokeWeight(4); //stroke wider
       line(currentTime,  currentTempOld, currentTime+timeInc, newTempOld);
@@ -60,13 +58,12 @@ function setup() {
   }
   
   if(sessionStorage.getItem("plasticTemperature") == 'true'){
-    console.log("plastic");
+    
 
     while(currentTime < windowWidth){
 
       var newTempOld = sessionStorage.getItem("plastic:"+currentTime);
-      console.log("here");
-      console.log(newTempOld);
+      
       stroke(186,217,68); //stroke color
       strokeWeight(4); //stroke wider
       line(currentTime,  currentTempOld, currentTime+timeInc, newTempOld);
@@ -83,13 +80,12 @@ function setup() {
   }
 
   if(sessionStorage.getItem("woodTemperature") == 'true'){
-    console.log("wood");
+    
     while(currentTime < windowWidth){
 
       // hashItem+":"+currentTime
       var newTempOld = sessionStorage.getItem("wood:"+currentTime);
-      console.log("here");
-      console.log(newTempOld);
+      
 
       stroke(249,105,73); //stroke color
       strokeWeight(4); //stroke wider
@@ -124,115 +120,8 @@ function draw() {
   // background(255);
   // console.log("tempVal1");
   // console.log(tempVal1);
-  var hash = (window.location.hash); 
-  var hashItem = hash.substring(1);
-
-  finalData = Number.parseFloat(finalData).toFixed(2);
-  // finalData =20;
-
-  var newTemp = maxHeight - (finalData - minTemperature)*(maxHeight - minHeight)/(maxTemperature - minTemperature);
-
-  if(hashItem =="steel"){
-    stroke(0,204,255, 200); //stroke color
-    strokeWeight(4); //stroke wider
-    line(currentTime,  currentTemp, currentTime+timeInc, newTemp);
-
-    var value = windowHeight- newTemp + "px";
-    if(document.getElementById('line') != null){
-      var dottedLine = document.getElementById('line');
-      // console.log((windowHeight- newTemp).isFinite());
-
-      if(isFinite(newTemp) == true){
-        // console.log("here");
-        dottedLine.x1.baseVal.value = 160;
-        dottedLine.y1.baseVal.value = parseFloat( newTemp);
-        dottedLine.x2.baseVal.value = 165+currentTime+timeInc;
-        dottedLine.y2.baseVal.value = parseFloat(newTemp);
-      }
-    }
-    fill(0,204,255, 120);
-    stroke(0,204,255, 3);
-    rect(currentTime,currentTemp,timeInc, maxHeight - newTemp);
-  }
-
-  if(hashItem =="plastic"){
-    stroke(186,217,68, 200); //stroke color
-    strokeWeight(4); //stroke wider
-    line(currentTime,  currentTemp, currentTime+timeInc, newTemp);
-
-    var value = windowHeight- newTemp + "px";
-    if(document.getElementById('line') != null){
-      var dottedLine = document.getElementById('line');
-      // console.log((windowHeight- newTemp).isFinite());
-
-      if(isFinite(newTemp) == true){
-        // console.log("here");
-        dottedLine.x1.baseVal.value = 160;
-        dottedLine.y1.baseVal.value = parseFloat( newTemp);
-        dottedLine.x2.baseVal.value = 165+currentTime+timeInc;
-        dottedLine.y2.baseVal.value = parseFloat(newTemp);
-      }
-    }
-    fill(186,217,68, 120);
-    stroke(186,217,68, 3);
-    rect(currentTime,currentTemp,timeInc, maxHeight - newTemp);
-  }
-
-  if(hashItem == "wood"){
-    stroke(249,105,73, 200); //stroke color
-    strokeWeight(4); //stroke wider
-    line(currentTime,  currentTemp, currentTime+timeInc, newTemp);
-
-    var value = windowHeight- newTemp + "px";
-    if(document.getElementById('line') != null){
-      var dottedLine = document.getElementById('line');
-      // console.log((windowHeight- newTemp).isFinite());
-
-      if(isFinite(newTemp) == true){
-        // console.log("here");
-        dottedLine.x1.baseVal.value = 160;
-        dottedLine.y1.baseVal.value = parseFloat( newTemp);
-        dottedLine.x2.baseVal.value = 165+currentTime+timeInc;
-        dottedLine.y2.baseVal.value = parseFloat(newTemp);
-      }
-    }
-    fill(249,105,73, 120);
-    stroke(249,105,73, 3);
-    rect(currentTime,currentTemp,timeInc, maxHeight - newTemp);
-  }
-
   
 
-  currentTemp = newTemp;
-  currentTime = currentTime+timeInc;
-
-  if (currentTime>582){
-    console.log("here");
-    timeIsHalf = true;
-  }
-
-  if(timeIsHalf == true){    
-    
-  }
-
-  var obj = {
-    item: hashItem,
-    currentTime: currentTime,
-  }
-
-  var objJSON = JSON.stringify(obj);
-
-
-  // stop after maxwidth
-  if(currentTime>windowWidth){
-    
-    window.location.href = "success.html" + hash;
-    sessionStorage.setItem(hashItem + "Temperature", "true");
-  }
-  else{
-    sessionStorage.setItem(hashItem+":"+currentTime, currentTemp);
-    numberOfReading++;
-  }
 }
 
 function drawScale(){
@@ -306,74 +195,3 @@ function setGradient(x, y, w, h, c1, c2, axis) {
   }
 }
 
-function Particle(){
-  // member properties and initialization
-  this.vel = createVector(0, 0);
-  this.pos = createVector(random(0, width), random(0, height));
-  this.life = random(0, (maxLife*this.modifier));
-  this.flip = int(random(0,2)) * 2 - 1;
-  // this.color1 = this.color2 = color('blue');
-
-  this.color1 = color_from;
-  this.color2 = color_to;
-
-  this.initialX = 0;
-  this.finalX = 0;
-  this.item = 0;
-  this.modifier = 0;
-  this.intitalY = 0;
-  this.finalY = 0;
-  // if(int(random(3)) == 1){
-  //   //this.color1 = color('palegreen');
-  //   //this.color2 = color('cyan');
-  //   this.color1 = color_from;
-  //   this.color2 = color_to;
-  // }
-  
-  // member functions
-  this.move = function(iterations){
-    if((this.life -= 0.01666) < 0)
-      this.respawnTop();
-    while(iterations > 0){
-      
-      var transition = map(this.pos.x, this.initialX, this.finalX, 0, 1);
-      var angle = noise(this.pos.x/noiseScale, this.pos.y/noiseScale)*transition*TWO_PI*noiseScale;
-      //var transition = map(this.pos.y, height/5, height-padding_top, 0, 1, true);
-      //var angle = HALF_PI;
-      //angle += (noise(this.pos.x/noiseScale, this.pos.y/noiseScale)-0.5)*transition*TWO_PI*noiseScale/66;
-
-      this.vel.x = cos(angle);
-      this.vel.y = sin(angle);
-      this.vel.mult(simulationSpeed*(tempReading[3][this.item] - tempReading[0][this.item])*windowHeight/100);
-      this.pos.add((this.vel));
-      --iterations;
-    }
-  }
-
-  this.checkEdge = function(){
-    if(this.pos.x > this.finalX
-    || this.pos.x < this.initialX
-    || this.pos.y > this.finalY
-    || this.pos.y < this.intitalY){
-      this.respawnTop();
-    }
-  }
-  
-  this.respawn = function(){
-    this.pos.x = random(0, width);
-    this.pos.y = random(0, height);
-    this.life = maxLife;
-  }
-  
-  this.respawnTop = function() {
-    this.pos.x = random(this.initialX, this.finalX);
-    this.pos.y = this.intitalY;
-    this.life = maxLife;
-    //this.color1 = lerpColor(color('white'), color_from, (this.pos.x-padding_side)/inner_square);
-    //this.color2 = lerpColor(color('white'), color_to, (this.pos.x-padding_side)/inner_square);
-  }
-
-  this.display = function(r){
-    ellipse(this.pos.x, this.pos.y, r, r);
-  }
-}
